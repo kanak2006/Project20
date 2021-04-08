9cfa978f4700979b65f13392f14c14c6fcbae903
kanak2006 <80760364+kanak2006@users.noreply.github.com>
2021-04-08 14:49:03 +0530

    Add files via upload

--- tomAndJerryTemplate-master/sketch.js
+++ tomAndJerryTemplate-master/sketch.js
@@ -1,19 +1,25 @@
-var cat,catImg;
+var cat,catImage;
 var mouse;
-
+var garden,gardenImage;
 
 function preload() {
     //load the images here
-catImg=loadImg("cat1.png");
-
+catImage=loadImage("images/cat1.png")
+gardenImage=loadImage("images/garden.png")
+mouseImage=loadImage("images/mouse1.png")
 }
 
 function setup(){
     createCanvas(1000,800);
     //create tom and jerry sprites here
-cat = createSprite(500,400,20,20);
-catImg.addImg=("cat",catImg);
-mouse = createSprite(100,200,10,10);
+    cat = createSprite(700,400,20,20);
+    cat.addImage(catImage);
+    cat.scale=0.5;
+    mouse = createSprite(100,200,10,10);
+    mouse.addImage(mouseImage);
+    mouse.scale=0.3;
+garden = createSprite(480,500,20,20);
+garden.addImage(gardenImage);
 }
 
 function draw() {
